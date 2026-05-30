3f151640629d2bee81757eadbcf3277a7d4b4967
imlyzh <enterindex@gmail.com>
2026-05-30 15:07:05 +0800

    Refactor IR types and add serde serialization support

--- src/lib.rs
+++ src/lib.rs
@@ -1,78 +1,77 @@
 #![no_std]
 extern crate alloc;
 
-pub mod common;
+use alloc::{string::String, vec::Vec};
+use serde::{Deserialize, Serialize};
 
-use alloc::vec::Vec;
-
-use crate::common::{RawType, Sign};
-
-#[derive(Debug)]
-pub struct FCIR {}
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+#[derive(Deserialize, Serialize, Debug, Clone, Copy, PartialEq, Eq, Hash)]
+#[repr(transparent)]
 pub struct NodeId(pub u32);
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+#[derive(Deserialize, Serialize, Debug, Clone, Copy, PartialEq, Eq, Hash)]
+#[repr(transparent)]
 pub struct ContId(pub u32);
+#[derive(Deserialize, Serialize, Debug, Clone, Copy, PartialEq, Eq, Hash)]
+#[repr(transparent)]
+pub struct DataId(pub u32);
+#[derive(Deserialize, Serialize, Debug, Clone, Copy, PartialEq, Eq, Hash)]
+#[repr(transparent)]
+pub struct ExternId(pub u32);
 
-#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+#[derive(Deserialize, Serialize, Debug, Clone)]
 pub struct FlatContGraph {
+    pub target_arch: u8,
+
+    pub static_data: Vec<StaticData>,
+    pub externals: Vec<ExternDecl>,
+
     pub nodes: Vec<Node>,
-    pub continuations: Vec<Continuation>,
-    pub entry: ContId,
+    pub continuations: Vec<FlatContinuation>,
+
+    pub exports: Vec<ExportedSymbol>,
 }
 
-#[derive(Debug, Clone, PartialEq, Eq, Hash)]
-pub struct Continuation {
-    pub param_count: usize,
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct ExportedSymbol {
+    pub name: String,
+    pub cont: ContId,
+}
+
+#[derive(Deserialize, Serialize, Debug, Clone, Copy, PartialEq, Eq, Hash)]
+#[repr(u8)]
+pub enum Visibility {
+    Private,
+    Public,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct StaticData {
+    pub name: String,
+    pub visibility: Visibility,
+    pub is_read_only: bool,
+    pub alignment: u8,
+    pub bytes: Vec<u8>,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct ExternDecl(pub String);
+
+#[derive(Deserialize, Serialize, Debug, Clone, PartialEq, Eq, Hash)]
+pub struct FlatContinuation {
+    pub name: Option<String>,
+    pub param: Vec<RawType>,
+    // pub node_start: NodeId,
+    // pub node_count: NodeId,
     pub contained_nodes: Vec<NodeId>,
     pub terminator: Terminator,
 }
 
-#[derive(Debug, Clone, PartialEq, Eq, Hash)]
-pub enum Node {
-    Const(i64),
-    Param(usize),
-    Proj(NodeId, u16),
-    // load store
-    Load {
-        effect_state: NodeId,
-        data_type: RawType,
-        addr: NodeId,
-    },
-    Store {
-        effect_state: NodeId,
-        data_type: RawType,
-        addr: NodeId,
-        value: NodeId,
-    },
-
-    // Compute
-    Add(NodeId, NodeId),
-    Sub(NodeId, NodeId),
-    Mul(NodeId, NodeId),
-    Div(Sign, NodeId, NodeId),
-    Rem(Sign, NodeId, NodeId),
-
-    And(NodeId, NodeId),
-    Or(NodeId, NodeId),
-    Xor(NodeId, NodeId),
-    Not(NodeId),
-
-    ShiftLeft(NodeId, NodeId),
-    ShiftRight(Sign, NodeId, NodeId),
-
-    CmpEq(NodeId, NodeId),
-    CmpNe(NodeId, NodeId),
-
-    CmpGt(Sign, NodeId, NodeId),
-    CmpLe(Sign, NodeId, NodeId),
-
-    Min(Sign, NodeId, NodeId),
-    Max(Sign, NodeId, NodeId),
+#[derive(Deserialize, Serialize, Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub enum RawType {
+    Ptr,
+    Scalar(u16), // bit
 }
 
-#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+#[derive(Deserialize, Serialize, Debug, Clone, PartialEq, Eq, Hash)]
 pub enum Terminator {
     Jump {
         target: ContId,
@@ -90,4 +89,86 @@ pub enum Terminator {
         args: Vec<NodeId>,
     },
     Return(Vec<NodeId>),
+}
+
+#[derive(Deserialize, Serialize, Debug, Clone, PartialEq, Eq, Hash)]
+pub enum Node {
+    Const(i64),
+    DataRef(DataId),
+    ContRef(ContId),
+    // load store
+    Load {
+        data_type: RawType,
+        signed: bool,
+        effect_state: NodeId,
+        addr: NodeId,
+    },
+    Store {
+        data_type: RawType,
+        signed: bool,
+        effect_state: NodeId,
+        addr: NodeId,
+        value: NodeId,
+    },
+    // Compute
+    Icmp(ICond, NodeId, NodeId),
+
+    Unary(Opcode, NodeId),
+    Binary(Opcode, NodeId, NodeId),
+    Ternary(Opcode, NodeId, NodeId, NodeId),
+    Call {
+        target: ContId,
+        args: Vec<NodeId>,
+    },
+}
+
+#[derive(Deserialize, Serialize, Debug, Clone, PartialEq, Eq, Hash)]
+#[repr(u16)]
+pub enum ICond {
+    Eq,
+    Ne,
+    Gt,
+    Ge,
+    Lt,
+    Le,
+    GtSign,
+    GeSign,
+    LtSign,
+    LeSign,
+}
+
+#[derive(Deserialize, Serialize, Debug, Clone, PartialEq, Eq, Hash)]
+#[repr(u16)]
+pub enum Opcode {
+    Invalid = 0,
+    Add,
+    Sub,
+    Mul,
+    Div,
+    DivSign,
+    Rem,
+    RemSign,
+
+    And,
+    Or,
+    Xor,
+    Not,
+
+    Shl,
+    Shr,
+    ShrSign,
+    Eq,
+    Ne,
+
+    Lt,
+    Le,
+    Gt,
+    Ge,
+
+    Minu,
+    Mins,
+    Maxu,
+    Maxs,
+
+    Select,
 }
