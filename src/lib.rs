b05fb0386ae6f0ed0f8e4ab9316fad6835c205cc
imlyzh <enterindex@gmail.com>
2026-06-07 06:58:36 +0800

    Add operator parameter to GEP variant

--- src/lib.rs
+++ src/lib.rs
@@ -134,7 +134,7 @@ pub enum Node {
         operator: AtomicRMWCode,
     },
     // Compute
-    GEP(RawType, SmallVec<[NodeId; 3]>),
+    GEP(RawType, NodeId, SmallVec<[NodeId; 3]>),
     Icmp(ICond, NodeId, NodeId),
     Compute(Opcode, SmallVec<[NodeId; 4]>),
     Call {
